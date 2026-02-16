7787d21bbcf8b8f38546476351ce74c31068e2b5
suman-ghorai1 <sumanghorai11744@gmail.com>
2026-02-17 01:23:44 +0530

    Directories and files created successfully

---
+++ template.sh
@@ -0,0 +1,16 @@
+#create directories for source code and research
+mkdir -p src
+mkdir -p research
+
+#create files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directories and files created successfully."
